9b7d34180ace9541362da28769b3e18b8d29ea5a
Barath005 <barathganesan01@gmail.com>
2024-05-27 12:09:10 +0530

    login and signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListingComponent } from './listing/listing.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent},
-  { path: 'listing', component: ListingComponent}
+  { path: '', component: LandingPageComponent },  // Default route
+  { path: 'listing', component: ListingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '/login' }  // Wildcard route to handle undefined routes
 ];
 
 @NgModule({
