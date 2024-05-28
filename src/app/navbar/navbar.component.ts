bab08fbda692e65ed02b649a8aec95e796d562e3
Jayagopal-web <jayagopal369@gmail.com>
2024-05-28 18:16:22 +0530

    profile Image

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,6 +11,14 @@ export class NavbarComponent implements OnInit {
 
   showSearchBar: boolean = false;
   searchvalue:string="";
+  userId:string | null = '';
+  LoginText:string = 'Login';
+  formData:any;
+  fname:string | null ='';
+  lname:string | null ='';
+  userName:string ='';
+  userProfile:string = 'user-profile-img profile';
+
   constructor(private router: Router,private httpClient: HttpClient,private sharedService:SharedService) {}
 
   ngOnInit() {
@@ -20,9 +28,34 @@ export class NavbarComponent implements OnInit {
         this.showSearchBar = event.urlAfterRedirects.includes('/listing');
       }
     });
+
+    this.userId = sessionStorage.getItem('ID');
+    this.fname = sessionStorage.getItem('firstName');
+    this.lname = sessionStorage.getItem('lastName');
+
+    const formDataString = localStorage.getItem('formData');
+    this.formData = formDataString ? JSON.parse(formDataString) as FormData : null;
+    
+    
+
+    if(this.userId!=null && this.fname!=null && this.lname!=null){
+      this.LoginText = 'Logout';
+      const firstLetterFname = this.fname.charAt(0).toUpperCase();
+      const firstLetterLname = this.lname.charAt(0).toUpperCase();
+      this.userName = `${firstLetterFname}${firstLetterLname}`;    
+      
+      this.userProfile = 'user-profile-img profile block';
+    }
   }
   onSearch(event: any) {
     const searchTerm = event.target.value;
     this.sharedService.updateSearchTerm(searchTerm);
+  }
+  loginBtn(){
+    if(this.userId!=null){
+      this.formData[this.userId].login= 'inactive';
+      localStorage.setItem('formData', JSON.stringify(this.formData));
+      sessionStorage.clear();
+    }
   }
 }
