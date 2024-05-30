15de5985b527837fa0f2ad240ff6179ad5a623b2
manikumarancse <manikumarancse@gmail.com>
2024-05-30 12:28:28 +0530

    added button to the cart componet to navigate to other pages

--- src/app/shared/constant/data.model.ts
+++ src/app/shared/constant/data.model.ts
@@ -43,4 +43,10 @@ export interface Product {
     barcode: string;
     qrCode: string;
   }
+  export interface CartItem {
+    productId: string;
+    productName: string;
+    quantity: number;
+    price: number;
+  }
   
